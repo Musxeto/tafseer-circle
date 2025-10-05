59b88cd977a47c3b175c649cb1065334ebbcadb9
musxeto <mustafamalikawan786@gmail.com>
2025-10-05 23:30:07 +0500

    set up pages and routes

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Join from "./pages/Join";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const AppRoutes: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/join" element={<Join />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRoutes;
---
+++ src/pages/Join.tsx
@@ -0,0 +1,10 @@
+const Join = () => {
+    return (
+        <div>
+            <h1>Join Our Team</h1>
+            <p>We are excited to have you on board! Fill out the form below to join our team.</p>
+        </div>
+    );
+};
+
+export default Join;
